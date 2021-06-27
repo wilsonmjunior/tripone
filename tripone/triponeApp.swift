07a3a9cb7533dfd835a6e8ea3caa41339c873484
wilsonmjunior <wilsonmjuniorx@gmailcom>
2021-06-26 22:53:48 -0300

    Initial Commit

---
+++ tripone/triponeApp.swift
@@ -0,0 +1,17 @@
+//
+//  triponeApp.swift
+//  tripone
+//
+//  Created by Wilson Junior on 26/06/21.
+//
+
+import SwiftUI
+
+@main
+struct triponeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
